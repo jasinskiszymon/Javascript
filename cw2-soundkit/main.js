884cbd96c4b9c32826c2ebd8e1cb572f5a016b95
jasinskiszymon <62395052+jasinskiszymon@users.noreply.github.com>
2020-12-10 11:32:15 +0100

    small changes

--- cw2-soundkit/main.js
+++ cw2-soundkit/main.js
@@ -1,60 +1,158 @@
-
+let channel = [];
+let channel2 =[];
+let channel3 = [];
+let channel4 = [];
+let recordStart;
+let recordStart2;
+let recordStart3;
+let recordStart4;
+let Recording1 = false;
+let Recording2 = false;
+let Recording3 = false;
+let Recording4 = false;
 document.body.addEventListener('keypress', onKeyPress);
-document.querySelector('#playBtn').addEventListener('click', playChannel);
-const channel = [];
-const recordStart = Date.now();
+document.querySelector('#playBtn').addEventListener('click', playChannel1);
+document.querySelector('#record').addEventListener('click', recordChannel);
+document.querySelector('#playBtn2').addEventListener('click', playChannel2);
+document.querySelector('#record2').addEventListener('click', record2Channel);
+document.querySelector('#playBtn3').addEventListener('click', playChannel3);
+document.querySelector('#record3').addEventListener('click', record3Channel);
+document.querySelector('#playBtn4').addEventListener('click', playChannel4);
+document.querySelector('#record4').addEventListener('click', record4Channel);
 function onKeyPress(ev) {
     let sound;
-    let soundName;
     switch (ev.code) {
         case 'KeyA':
-            soundName = 'boom';
-            sound = document.querySelector('#boom');
+            sound = 'boom';
             break;
         case 'KeyS':
-            soundName = 'clap';
-            sound = document.querySelector('#clap');
+            sound = 'clap';
             break;
         case 'KeyD':
-            soundName = 'hihat';
-            sound = document.querySelector('#hihat');
+            sound = 'hihat';
+           
             break;
         case 'KeyF':
-            soundName = 'kick';
-            sound = document.querySelector('#kick');
+            sound = 'kick';
+           
             break;
         case 'KeyG':
-            soundName = 'tom';
-            sound = document.querySelector('#tom');
+            sound = 'tom';
+          
             break;
         case 'KeyZ':
-            soundName = 'openhat';
-            sound = document.querySelector('#openhat');
+            sound = 'openhat';
+           
             break;
         case 'KeyX':
-            soundName = 'ride';
-            sound = document.querySelector('#ride');
+            sound = 'ride';
             break;
         case 'KeyC':
-            soundName = 'snare';
-            sound = document.querySelector('#snare');
+            sound = 'snare';
+            
             break; 
         case 'KeyV':
-            soundName = 'tink';
-            sound = document.querySelector('#tink');
+            sound = 'tink';
+            
             break;
     }
+
     if (sound) {
         const keyPressTime = Date.now() - recordStart;
-        const recordedSound = {
-            sound: soundName,
-            time: keyPressTime
+        const soundObject = {
+            sound: sound,
+            time1: keyPressTime
         };
-        channel.push(recordedSound);
-        sound.play();
+        playSound(sound);
+        if(Recording1 == true){
+            channel.push(soundObject);
+
+        }
+}
+    if (sound) {
+    const keyPressTime = Date.now() - recordStart2;
+    const soundObject2 = {
+        sound: sound,
+        time2: keyPressTime
+    }
+    playSound(sound)
+    if(Recording2 == true){
+    channel2.push(soundObject2);
     }
 }
-function playChannel() {
+
+if (sound) {
+    const keyPressTime = Date.now() - recordStart3;
+    const soundObject3 = {
+        sound: sound,
+        time3: keyPressTime
+    }
+    playSound(sound)
+    if(Recording3 == true){
+    channel3.push(soundObject3);
+    }
+}
+if (sound) {
+    const keyPressTime = Date.now() - recordStart4;
+    const soundObject4 = {
+        sound: sound,
+        time4: keyPressTime
+    }
+    playSound(sound)
+    if(Recording4 == true){
+    channel4.push(soundObject4);
+    }
+}
+}
+function recordChannel() {
+    if(Recording1==false)
+    {
+        Recording1 = false;
+    }
+    else
+    {
+        Recording1 = true;
+        channel = [];
+        recordStart = Date.now();
+    }
+}
+function record2Channel() {
+    if(Recording2)
+    {
+        Recording2 = false;
+    }
+    else
+    {
+        Recording2 = true;
+        channel2 = [];
+        recordStart2 = Date.now();
+    }
+}
+function record3Channel() {
+    if(Recording3)
+    {
+        Recording3 = false;
+    }
+    else
+    {
+        Recording3 = true;
+        channel3 = [];
+        recordStart3 = Date.now();
+    }
+}
+function record4Channel() {
+    if(Recording4)
+    {
+        Recording4 = false;
+    }
+    else
+    {
+        Recording4 = true;
+        channel4 = [];
+        recordStart4 = Date.now();
+    }
+}
+function playChannel1() {
     for (let index = 0; index < channel.length; index++) {
         const soundObject = channel[index];
         setTimeout(
@@ -66,7 +164,45 @@ function playChannel() {
     }
 
 }
-function playSound(soundName) {
-    const sound = document.querySelector('#' + soundName);
-    sound.play();
+
+function playChannel2() {
+    for (let index = 0; index < channel2.length; index++) {
+        const soundObject2 = channel2[index];
+        setTimeout(
+            () => {
+                playSound(soundObject2.sound);
+            },
+            soundObject2.time2
+        );
+    }
+
+}
+function playChannel3() {
+    for (let index = 0; index < channel3.length; index++) {
+        const soundObject3 = channel3[index];
+        setTimeout(
+            () => {
+                playSound(soundObject3.sound);
+            },
+            soundObject3.time3
+        );
+    }
+
+}
+function playChannel4() {
+    for (let index = 0; index < channel4.length; index++) {
+        const soundObject4 = channel4[index];
+        setTimeout(
+            () => {
+                playSound(soundObject4.sound);
+            },
+            soundObject4.time4
+        );
+    }
+
+}
+
+function playSound(sound) {
+    const soundplay = document.querySelector('#' + sound);
+    soundplay.play();
 }
